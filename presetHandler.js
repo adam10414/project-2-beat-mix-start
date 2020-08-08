f2c4d3a301be8be895a96edfef771e379f2da930
Waylon <68206719+adam10414@users.noreply.github.com>
2020-08-08 09:58:48 -0500

    My first 404 response

--- presetHandler.js
+++ presetHandler.js
@@ -1,8 +1,37 @@
 // Use this presets array inside your presetHandler
+//Array format: [0][0][0]
+//First Index = Which group of Arrays. Groups of arrays come in 4's. There are 4 groups. (Index 0-3)
+//Second Index = Which sub-array.
+//Third index = Which sub-element you want to choose. 
 const presets = require('./presets');
+const { response } = require('express');
 
 // Complete this function:
-const presetHandler = () => {};
+//This function will return an array based on the type of request. 
+//GET: 200, Returns the top array element to the client.  
+//PUT: 200, Modifies the top array element specified by the client. 
+//404: Is returned when a request is made out of bounds. 
+//400: Bad request is returned when the request did not come in the form of GET or PUT. 
+const presetHandler = (reqType, index, newPresetArray) => {
+    let repsonse = [];
+    
+    //404
+    if (index < 0 || index > 3){
+     response[0] = 404;
+     return response;
+    }
+    
+    
+    switch(reqType){
+        case 'GET':
+            
+
+
+    }//closing switch
+
+};//closing presetHandler
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
+
+console.log(presetHandler('meow', 7));
